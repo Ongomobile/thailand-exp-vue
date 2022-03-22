79ffd54d1a17966f0145645640b335668824a2b7
ongomobile <ongomobile@gmail.com>
2022-03-21 20:12:37 -0700

    Add YouTube & Google Maps packages

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,73 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <GoogleMap
+    api-key="AIzaSyAiiVKqAveppqhACuLuiEC1mYJIP93t6Mw"
+    style="width: 100%; height: 50vh"
+    :center="center"
+    :zoom="15"
+  >
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+  <div>
+    <div>
+      <button @click="playCurrentVideo">Play</button>
+      <button @click="stopCurrentVideo">Stop</button>
+      <button @click="pauseCurrentVideo">Pause</button>
+    </div>
+    <YoutubeVue3
+      ref="youtube"
+      :videoid="video_id"
+      :width="480"
+      :height="320"
+      @ended="onEnded"
+      @paused="onPaused"
+      @played="onPlayed"
+    />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+import { defineComponent } from "vue";
+import { GoogleMap, Marker } from "vue3-google-map";
+import { YoutubeVue3 } from "youtube-vue3";
+
+export default defineComponent({
+  components: { GoogleMap, Marker, YoutubeVue3 },
+  setup() {
+    const center = { lat: 14.99433, lng: 103.1039 };
+    return { center };
+  },
+  data() {
+    return {
+      video_id: "3P1CnWI62Ik",
+    };
+  },
+  methods: {
+    playCurrentVideo() {
+      this.$refs.youtube.player.playVideo();
+    },
+    stopCurrentVideo() {
+      this.$refs.youtube.player.stopVideo();
+    },
+    pauseCurrentVideo() {
+      this.$refs.youtube.player.pauseVideo();
+    },
+    onEnded() {
+      console.log("## OnEnded");
+    },
+    onPaused() {
+      console.log("## OnPaused");
+    },
+    onPlayed() {
+      console.log("## OnPlayed");
+    },
+  },
+});
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+select {
+  width: 200px;
+  margin-top: 20px;
+  margin-left: 10px;
 }
 </style>
