40fc602dcd2b0913753253861c6a8c48f6d760d4
ongomobile <ongomobile@gmail.com>
2022-03-22 15:44:19 -0700

    Add base componet functionality & AppSpinner to be implemented later

--- src/main.js
+++ src/main.js
@@ -4,4 +4,20 @@ import router from '@/router'
 
 const thalaindApp = createApp(App)
 thalaindApp.use(router)
+
+// Base componernt logic
+const requireComponent = require.context(
+  './components',
+  true,
+  /App[A-Z]\w+\.(vue|js)$/
+)
+requireComponent.keys().forEach(function (fileName) {
+  let baseComponentConfig = requireComponent(fileName)
+  baseComponentConfig = baseComponentConfig.default || baseComponentConfig
+  const baseComponentName =
+    baseComponentConfig.name ||
+    fileName.replace(/^.+\//, '').replace(/\.\w+$/, '')
+  thalaindApp.component(baseComponentName, baseComponentConfig)
+})
+
 thalaindApp.mount('#app')
