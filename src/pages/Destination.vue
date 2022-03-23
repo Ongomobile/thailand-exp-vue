456bd162e661bfd9562979658891af646ec5c895
ongomobile <ongomobile@gmail.com>
2022-03-23 15:20:01 -0700

    Fetch & set data from wikivoyage

--- src/pages/Destination.vue
+++ src/pages/Destination.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>Destination Page</h1>
+  <img :src="locationImgUrl" alt="" />
 </template>
 
 <script>
@@ -8,7 +9,11 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      pickedLocation: {}
+      pickedLocation: {},
+      locationTitle: '',
+      locationImgUrl: null,
+      locationDescription: '',
+      moreInfoLink: ''
     }
   },
   methods: {
@@ -17,21 +22,57 @@ export default {
       const random = Math.floor(Math.random() * locations.length)
       let randomLocation = locations[random]
       this.pickedLocation = randomLocation
-
-      console.log(this.pickedLocation)
+      this.locationTitle = this.pickedLocation.name
     },
     async getWikiData(location) {
       try {
         const response = await axios.get(
-          `https://en.wikivoyage.org/w/api.php?origin=*&action=query&prop=pageimages%7Cextracts&exlimit=1&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=250&pilimit=20&gpssearch=${location}&gpslimit=1`
+          `https://en.wikivoyage.org/w/api.php?origin=*&action=query&format=json&prop=pageimages%7Cextracts&exlimit=1&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=250&pilimit=20&gpssearch=${location}&gpslimit=1`
         )
-        console.log(response.data)
+        let wikiData = response.data
+        if (wikiData) {
+          this.setThumbnailImg(wikiData)
+        }
+        const htmlString = response.data.query.pages[0].extract
+        this.getDescriptionText(htmlString)
       } catch (error) {
         console.log({ error })
       }
+    },
+    getDescriptionText(data) {
+      let parser = new DOMParser()
+      const doc = parser.parseFromString(data, 'text/html')
+
+      const text = doc.getElementsByTagName('p')
+      const descriptionText =
+        text[0].innerText + text[1].innerText.substring(0, 320)
+
+      const formattedDescription = (this.locationDescription =
+        this.formatDescriptionText(descriptionText) + '...')
+      return formattedDescription
+    },
+    formatDescriptionText(str) {
+      const lastIndexOfSpace = str.lastIndexOf(' ')
+
+      if (lastIndexOfSpace === -1) {
+        return str
+      }
+      return str.substring(0, lastIndexOfSpace)
+    },
+    setThumbnailImg(data) {
+      // eslint-disable-next-line no-unused-vars
+      const imageUrl = data.query.pages.map((item, index) => {
+        // eslint-disable-next-line no-prototype-builtins
+        return item.hasOwnProperty('thumbnail')
+          ? item.thumbnail.source
+          : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/428394/chang.png'
+      })
+
+      this.locationImgUrl = imageUrl[0]
+      console.log(this.locationImgUrl)
     }
   },
-  mounted() {
+  created() {
     this.getLocationData()
     this.getWikiData(this.pickedLocation.name)
   }
@@ -39,4 +80,8 @@ export default {
 </script>
 
 <style>
+img {
+  height: 250px;
+  width: 250px;
+}
 </style>
