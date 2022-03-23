09499e7a7716546e867cb1ac654afbf6528ed4c1
ongomobile <ongomobile@gmail.com>
2022-03-22 21:08:09 -0700

    Add axios

--- src/pages/Destination.vue
+++ src/pages/Destination.vue
@@ -3,7 +3,26 @@
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    getLocationData() {
+      try {
+        this.axios
+          .get(
+            'https://raw.githubusercontent.com/Ongomobile/thailand-explorer/master/public/locations.json'
+          )
+          .then((response) => {
+            console.log(response.data)
+          })
+      } catch (error) {
+        console.log({ error })
+      }
+    }
+  },
+  mounted() {
+    this.getLocationData()
+  }
+}
 </script>
 
 <style>
