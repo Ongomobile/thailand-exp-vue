3a6fd01c5d559cc68056df711cda3c1896e72571
ongomobile <ongomobile@gmail.com>
2022-03-24 18:22:54 -0700

    Add styles

--- src/pages/Destination.vue
+++ src/pages/Destination.vue
@@ -1,14 +1,67 @@
 <template>
   <div v-if="asyncDataStatus_ready" class="container">
     <DestinationNav />
-    <div class="destination-map-wrapper">
-      <h2 class="destination-title">Explore {{ locationTitle }}</h2>
-      <GoogleMap :lat="lat" :lng="lng" />
-    </div>
-    <div class="destination-video-wrapper">
-      <YouTube :video_id="vidId" />
-    </div>
-    <img :src="locationImgUrl" alt="" />
+    <main>
+      <div class="destination-map-wrapper">
+        <h2 class="destination-title">Explore {{ locationTitle }}</h2>
+        <GoogleMap :lat="lat" :lng="lng" />
+      </div>
+      <div class="destination-center-content">
+        <div class="video-wrapper">
+          <YouTube :video_id="vidId" />
+        </div>
+        <div class="destination-pegman-wrapper">
+          <img
+            src="https://res.cloudinary.com/dch4i7gjv/image/upload/v1648164955/pegman-gif_tepdwj.gif"
+            alt="pegman"
+            class="pegman-img"
+          />
+          <div class="pegman-text-content">
+            <h3 class="pegman-heading">
+              Hi, Want to see<br />stuff on the map?
+            </h3>
+            <ol class="pegman-list">
+              <li class="list-item">In bottom right corner of map zoom in.</li>
+              <li class="list-item">Drag the pegman to the area to explore.</li>
+              <li class="list-item">Drop pegman on blue line or blue dot.</li>
+              <li class="list-item">
+                To go back click back arrow top left of map.
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+      <div class="destination-bottom-content-wrapper">
+        <div class="destination-img-wrapper">
+          <img
+            :src="locationImgUrl"
+            alt="destination image"
+            class="destination-img"
+          />
+        </div>
+        <div class="destination-text-content">
+          {{ locationDescription }}
+          <a
+            class="moreLink"
+            target="_blank"
+            href="https://en.wikivoyage.org/wiki/${location}"
+            >More information about {{ locationTitle }} from WikiVoyage
+            &hellip;</a
+          >
+        </div>
+      </div>
+
+      <div class="policies-wrapper">
+        <a href="https://www.youtube.com/t/terms" class="policy-link"
+          >YouTube terms of service</a
+        >
+        <a
+          href="https://policies.google.com/privacy?hl=en-US"
+          class="policy-link"
+          >Google privacy policy</a
+        >
+      </div>
+    </main>
   </div>
 </template>
 
@@ -26,7 +79,7 @@ export default {
     return {
       pickedLocation: {},
       locationTitle: '',
-      locationImgUrl: null,
+      locationImgUrl: '',
       locationDescription: '',
       moreInfoLink: '',
       lat: '',
@@ -117,9 +170,106 @@ export default {
   color: var(--headline-clr);
   margin-bottom: 0.8rem;
 }
-img {
+
+.destination-map-wrapper {
+  padding: 2rem;
+  margin-top: 2rem;
+  margin-bottom: 5.312rem;
+  box-shadow: 1px 1px 3px 0 rgb(55 64 71 / 66%);
+}
+
+.destination-center-content {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 5.312rem;
+}
+
+.pegman-text-content {
+  display: flex;
+  flex-direction: column;
+}
+.destination-pegman-wrapper {
+  display: flex;
+  align-items: center;
+  padding: 2rem;
+  box-shadow: 1px 1px 3px 0 rgb(55 64 71 / 66%);
+}
+
+.pegman-heading {
+  margin-left: 1.5rem;
+}
+
+.destination-text-content {
+  box-shadow: 1px 1px 3px 0 rgb(55 64 71 / 66%);
+  background-color: #fff;
+  max-width: 530px;
+  padding: 1.8rem;
+}
+
+.destination-bottom-content-wrapper {
+  display: flex;
+  margin-bottom: 5.312rem;
+}
+.destination-img-wrapper {
   height: 250px;
   width: 250px;
+  margin-right: 2rem;
+  box-shadow: 1px 1px 3px 0 rgb(55 64 71 / 66%);
+}
+.destination-img {
+  width: 100%;
+  height: 100%;
+}
+.policies-wrapper {
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem;
+}
+
+.pegman-img {
+  margin-right: 2rem;
+}
+
+.moreLink {
+  margin-top: 10px;
+}
+
+@media screen and (max-width: 768px) {
+  .destination-center-content {
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;
+  }
+  .conatiner {
+    max-width: 100vw;
+  }
+  .destination-pegman-wrapper {
+    max-width: 80vw;
+    margin-bottom: 5.312rem;
+  }
+
+  .destination-bottom-content-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .destination-img-wrapper {
+    margin-bottom: 5.312rem;
+  }
+  .destination-img-wrapper {
+    margin: 0 0 2rem;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .destination-pegman-wrapper {
+    flex-direction: column;
+  }
+
+  .pegman-img {
+    margin: 0 0 1rem;
+  }
 }
 </style>
 
