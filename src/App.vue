b5a2e72589355408cf9d17711829c4ffaba8a365
ongomobile <ongomobile@gmail.com>
2022-03-25 22:00:06 -0700

    Og image test

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <AppHead>
-    <title>Thailand Explorer</title>
-    <meta
-      name="description"
-      content="Explore cool random destinations in Thailand,places you never thought about visiting"
-    />
+    <title>Vue.js 3 Master Class Forum</title>
+    <meta name="description" content="An awesome Vue.js 3 powered forum!" />
 
     <!-- Social -->
-    <meta property="og:title" content="Thailand Explorer" />
+    <meta property="og:title" content="Vue.js 3 Master Class Forum" />
     <meta
       property="og:description"
-      content="Explore cool random destinations in Thailand"
+      content="An Awesome Vue.js 3 powered forum!"
     />
     <meta
       property="og:image"
@@ -18,14 +15,14 @@
     />
 
     <!-- Twitter -->
-    <meta name="twitter:title" content="Thailand Explorer" />
+    <meta name="twitter:title" content="Vue.js 3 Master Class Forum" />
     <meta
       name="twitter:description"
-      content="Explore cool random destinations in Thailand"
+      content="An Awesome Vue.js 3 powered forum!"
     />
     <meta
       name="twitter:image"
-      content="https://res.cloudinary.com/dch4i7gjv/image/upload/v1648084976/thai-exp-og_defbfp.jpg"
+      content="https://vueschool.io/media/f007f6057444d9a7f567163391d2b366/vuejs-3-master-class-not-transparent.jpg"
     />
     <meta name="twitter:card" content="summary_large_image" />
   </AppHead>
